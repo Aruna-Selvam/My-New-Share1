8040f42507f837f17db39d2f941ee5c99dd8a3e3
Aruna <arunaselvam233@gmail.com>
2022-09-14 10:50:59 -0500

    New Changes

--- src/main/java/Aruna.java
+++ src/main/java/Aruna.java
@@ -1,5 +1,8 @@
 public class Aruna {
 
     public static void main(String[] args){
-    System.out.println("Aruna");}
+    System.out.println("Aruna");
+
+        System.out.println("fun");
+        System.out.println("Made Changes");}
 }
