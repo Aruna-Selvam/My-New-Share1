13ddd1dd04cff691caaf376129226ef2f4e24cd9
Aruna <arunaselvam233@gmail.com>
2022-09-14 10:53:09 -0500

    few

--- src/main/java/Aruna.java
+++ src/main/java/Aruna.java
@@ -2,7 +2,8 @@ public class Aruna {
 
     public static void main(String[] args){
     System.out.println("Aruna");
-
+int i=0;
+int f=1;
         System.out.println("fun");
         System.out.println("Made Changes");}
 }
